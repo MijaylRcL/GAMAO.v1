46392d953bd5e924c3c142afd38addba5cdf9f89
Mijayl RcL <mijayl.rcl@gmail.com>
2024-04-14 15:48:49 -0500

    cuarto

--- javascript/efectos.js
+++ javascript/efectos.js
@@ -6,6 +6,21 @@ hamburger.onclick = function() {
   navBar.classList.toggle("active");
 }
 
+/*----Header - sticky---*/
+
+const header_sticky = document.getElementById("header-sticky")
+
+window.onscroll = function () {
+  addHeaderSticky()
+}
+
+function addHeaderSticky() {
+  if (window.pageYOffset === 0) {
+    header_sticky.classList.remove("header-sticky")
+  } else {
+    header_sticky.classList.add("header-sticky")
+  }
+}
 
 /*----Contador---*/
 
